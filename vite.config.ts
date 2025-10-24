0193b39f8767a691fb292e359ee649219b0c4290
goni715 <halesgoni715149123@gmail.com>
2025-10-24 11:47:47 +0600

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+// import path from "path";
  
  
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 2500,
-  },
-  server: {
-    //host: '13.53.182.102',  
-    host: '0.0.0.0',
-    port: 5173   
-  },
-  preview: { 
-    allowedHosts: ['dashboard.triplemcompany.com', '16.16.183.92', "www.dashboard.triplemcompany.com"],  
-  },
+  // build: {
+  //   outDir: 'dist',
+  //   chunkSizeWarningLimit: 2500,
+  // },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
+  // server: {
+  //   //host: '13.53.182.102',  
+  //   host: '0.0.0.0',
+  //   port: 5173   
+  // },
+  // preview: { 
+  //   allowedHosts: ['dashboard.triplemcompany.com', '16.16.183.92', "www.dashboard.triplemcompany.com"],  
+  // },
 })
